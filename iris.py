3a9aa95c141ebbdc366e3087c02837861124a442
Andhikanurf <97925983+Andhikanurf@users.noreply.github.com>
2023-01-31 22:42:46 +0700

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import streamlit as st
+import pandas as pd
+import pickle
+#from PIL import Image
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.header("Iris Classification:")
+st.image = ('iris-dataset.png')
+
+SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
+SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
+PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
+PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
+data = {'SepalLengthCm': SepalLengthCm,
+        'SepalWidthCm': SepalWidthCm,
+        'PetalLengthCm': PetalLengthCm,
+        'PetalWidthCm': PetalWidthCm}
+
+features = pd.DataFrame(data, index=[0])
+
+pred_proba = model.predict_proba(features)
+#or
+prediction = model.predict(features)
+
+st.subheader('Prediction Percentages:') 
+st.write('**Probablity of Iris Class being Iris-setosa is ( in % )**:',pred_proba[0][0]*100)
+st.write('**Probablity of Isis Class being Iris-versicolor is ( in % )**:',pred_proba[0][1]*100)
+st.write('**Probablity of Isis Class being Iris-virginica ( in % )**:',pred_proba[0][2]*100)
+
+
+# In[ ]:
+
+
+
+
