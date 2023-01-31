f39040c90474902a08368d35450329cf3b631f0d
Andhikanurf <97925983+Andhikanurf@users.noreply.github.com>
2023-02-01 00:37:52 +0700

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,17 +1,40 @@
 import streamlit as st
 import pandas as pd
-import pickle
-#from PIL import Image
+import numpy as np
+import pickle #to load a saved model
+import base64 #to open .gif files in streamlit app
+
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.header("Iris Classification:")
-image = ('iris-dataset.png')
-st.write("Please insert values, to get Iris class prediction")
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):
+    feature_dict = {"No":1,"Yes":2}
+    for key,value in feature_dict.items():
+        if val == key:
+            return value
 
-SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
-SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
-PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
-PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
+def get_value(val,my_dict):
+    for key,value in my_dict.items():
+        if val == key:
+            return value
+        
+app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
+
+if app_mode=='Home':
+    st.title('IRIS CLASSIFICATION PREDICTION') 
+    st.markdown('Dataset :')
+    data=pd.read_csv('iris.csv')
+    st.write(data.head())
+    st.markdown('Iris Setosa VS Iris Versicolor VS Iris Virginica ')
+    st.bar_chart(data[['SepalLengthCm', 'SepalWidthCm','PetalLengthCm', 'PetalWidthCm']].head(20))
+
+elif app_mode == 'Prediction':
+    st.image('iris.png')
+    st.write("Please Insert Values, to Get Iris Classification Prediction")
+    SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
+    SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
+    PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
+    PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
 data = {'SepalLengthCm': SepalLengthCm,
         'SepalWidthCm': SepalWidthCm,
         'PetalLengthCm': PetalLengthCm,
